b0aae510d6aeab3cb80f19976204b5d39ad93de7
Mikael Edgren <mikael.edgren@whatwedo.se>
2019-05-22 21:55:39 +0200

    Fix media query syntax error

--- src/logic/Responsive/Responsive.ts
+++ src/logic/Responsive/Responsive.ts
@@ -4,8 +4,8 @@ import responsiveState from './Responsive.state';
 
 export class Responsive {
   init = (): void => {
-    enquire.register('screen and (max-width: 768px', this.onMobile);
-    enquire.register('screen and (min-width: 769px', this.onDesktop);
+    enquire.register('screen and (max-width: 768px)', this.onMobile);
+    enquire.register('screen and (min-width: 769px)', this.onDesktop);
   };
 
   private onMobile = (): void => {
