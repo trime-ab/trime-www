656fac258dec2bb15aed2c46602be27ca962c385
Anthony bassey <39740647+soulzidda@users.noreply.github.com>
2020-03-24 13:01:14 +0100

    update to wording
    Descrition of Trime amended

--- src/ui/components/HomeTopContent/HomeTopContent.tsx
+++ src/ui/components/HomeTopContent/HomeTopContent.tsx
@@ -1,32 +1,35 @@
-import './HomeTopContent.css';
+import "./HomeTopContent.css";
 
-import classnames from 'classnames';
-import { inject, observer } from 'mobx-react';
-import React from 'react';
+import classnames from "classnames";
+import { inject, observer } from "mobx-react";
+import React from "react";
 
-import ResponsiveProps from '../../../logic/Responsive/Responsive.props';
-import phonesAndDudeImg from '../../assets/phones-and-dude.png';
-import trimeImg from '../../assets/trime.png';
-import SignUp from '../SignUp/SignUp';
+import ResponsiveProps from "../../../logic/Responsive/Responsive.props";
+import phonesAndDudeImg from "../../assets/phones-and-dude.png";
+import trimeImg from "../../assets/trime.png";
+import SignUp from "../SignUp/SignUp";
 
-@inject('responsiveState')
+@inject("responsiveState")
 @observer
 class HomeTopContent extends React.Component<ResponsiveProps> {
   render() {
     const className = this.getClassNames;
 
     return (
-      <div className={className('top-content')}>
-        <div className={className('top-image-container')}>
-          <div className={className('top-image-wrapper')}>
+      <div className={className("top-content")}>
+        <div className={className("top-image-container")}>
+          <div className={className("top-image-wrapper")}>
             <img src={phonesAndDudeImg} alt="Trime" />
           </div>
         </div>
-        <div className={className('top-text-container')}>
+        <div className={className("top-text-container")}>
           <h1>Welcome to</h1>
-          <img src={trimeImg} alt="Trime" className={className('trime')} />
-          <div className={className('top-text')}>
-            Trime is an app for personal trainers & trainees. <br />
+          <img src={trimeImg} alt="Trime" className={className("trime")} />
+          <div className={className("top-text")}>
+            Trime is a mobile marketplace that simplifies the process of
+            finding, comparing, booking, administrating and handling
+            transactions. While also being able to follow up with the Personal
+            Training for both trainers and trainees. <br />
             Coming in 2020.
           </div>
           <SignUp />
